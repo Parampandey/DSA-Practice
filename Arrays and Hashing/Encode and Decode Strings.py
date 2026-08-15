d4d924fd7569af63aea626bc86e6c3b97b267520
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-08-16 00:12:07 +0530

    Encode and Decode of strings
    Implement encoding and decoding methods for a list of strings.

---
+++ Arrays and Hashing/Encode and Decode Strings.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        string=""
+        for s in strs:
+          string+=str(len(s))+'#'+s
+        return string
+
+    def decode(self, s: str) -> List[str]:
+        lst=[]
+        i=0
+        while i<len(s):
+            j=i
+            while s[j]!='#':
+                j=j+1
+            length=int(s[i:j])
+            lst.append(s[j+1:j+1+length])
+            i=j+1+length
+        return lst
+              
+
+      
