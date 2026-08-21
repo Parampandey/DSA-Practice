674054b2b63e5644f46ab6d4d502ca4606bb2417
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-08-21 22:43:37 +0530

    Valid Palindrome
    Implement a method to check if a string is a palindrome by ignoring spaces and punctuation.

---
+++ Arrays and Hashing/Valid Palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st=s.lower()
+        st=st.replace(" ","")
+        result=""
+        for ch in st:
+            if ch.isalnum():
+                result+=ch
+
+        i=0;
+        j=len(result)-1
+        while (i<=j):
+            if result[i]!=result[j]:
+                return False
+            i=i+1
+            j=j-1
+
+        return True
+
+            
