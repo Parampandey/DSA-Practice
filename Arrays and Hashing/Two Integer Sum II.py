7a1b0cbd9da6510d91e1be06db93215f0c26d6e1
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-08-27 12:39:33 +0530

    2 Sum

--- Arrays and Hashing/Two Integer Sum II.py
+++ Arrays and Hashing/Two Integer Sum II.py
@@ -3,7 +3,7 @@ class Solution:
        left=0
        right=len(numbers)-1
        while(left<right):
-        s = numbers[left] + numbers[right]
+        s = numbers[left] +  numbers[right]
         if s==target:
             return [left+1,right+1]
         elif s<target:
