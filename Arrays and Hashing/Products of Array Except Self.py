62737671a5f9e0ae00ffc0e9e503daae49a34f8c
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-08-28 22:33:41 +0530

    Products of Array Except Self

--- Arrays and Hashing/Products of Array Except Self.py
+++ Arrays and Hashing/Products of Array Except Self.py
@@ -2,7 +2,7 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         prefix=1
         res=[1]*len(nums)
-        for i in range(len(nums)):
+        for i in range(len(nums)) :
             res[i]=prefix
             prefix*=nums[i]
 
