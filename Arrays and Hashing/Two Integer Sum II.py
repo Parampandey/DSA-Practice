4a540e00b2cddfaf87519a196651ccd0e81b1f6e
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-08-30 17:46:59 +0530

    Two Integer Sum II.py

--- Arrays and Hashing/Two Integer Sum II.py
+++ Arrays and Hashing/Two Integer Sum II.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
        left=0
-       right=len(numbers)-1
+       right=len(numbers) -1
        while(left<right):
         s = numbers[left] +  numbers[right ]
         if s==target:
