3b289beacde147c3b475bf53999e23f31a0811ea
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-09-09 21:16:57 +0530

    Best Time to Buy and Sell Stock.py

--- Sliding Window/Best Time to Buy and Sell Stock.py
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int :
+    def maxProfit(self, prices: List[int]) -> int:
         left=0 # we use sliding window left pointer for buy the stock
         right=1 # right pointer is to sell the stock so profit right-left
         maxprofit=0
